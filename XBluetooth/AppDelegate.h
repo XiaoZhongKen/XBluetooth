2f8e40547e70ce1a827e4b02420d0a54a9aa6d16
xzk <1101520833@qq.com>
2019-06-21 15:29:18 +0800

    Initial Commit

---
+++ XBluetooth/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XBluetooth
+//
+//  Created by 肖忠肯 on 2019/6/21.
+//  Copyright © 2019 肖忠肯. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XBluetooth/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XBluetooth
+//
+//  Created by 肖忠肯 on 2019/6/21.
+//  Copyright © 2019 肖忠肯. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
